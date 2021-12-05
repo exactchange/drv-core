ce215826a3090e0d9e1eddff8360ca80e33ab82a
hello@bennyschmidt.com <hello@bennyschmidt.com>
2021-12-05 11:27:12 -0800

    cleaner log

--- enforcements/enforcements.standard.js
+++ enforcements/enforcements.standard.js
@@ -33,7 +33,7 @@ module.exports = async (transaction, peers) => {
     console.log('<Embercoin> :: There are no peers online to echo the transaction. It will remain in a pending state until the next enforcement attempt.');
   }
 
-  console.log(`<Embercoin> Transaction was corroborated by ${confidence} peers (Confidence: ${Math.min(99, Math.min(4, confidence) / 4 * 100)}%).`);
+  console.log(`<Embercoin> :: Transaction was corroborated by ${confidence} peers (Confidence: ${Math.min(99, Math.min(4, confidence) / 4 * 100)}%).`);
 
   return didConnectAndSend ? COMPLETE : PENDING;
 };
